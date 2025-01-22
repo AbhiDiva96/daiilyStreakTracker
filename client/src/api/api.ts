66ba2ec736e5e31305cf48ce596d2a86f5e791be
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2025-01-22 14:41:36 +0530

    backend deployment api

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/v1";
+const API_URL = "https://daiilystreaktracker.onrender.com/api/v1";
 
 export const api = axios.create({
       baseURL: API_URL
