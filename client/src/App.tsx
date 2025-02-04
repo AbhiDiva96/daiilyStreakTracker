0646c646c5f3d342861e2a6b8708e95c5f4716f0
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2025-02-04 14:51:18 +0530

    anaylytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { StreakTracker } from './components/trackStreak';
 import { Home } from './components/home';
 import { LandingPage } from './components/landing';
+import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
       
   return(
       <div>
+        <Analytics/>
         <Router >
             <Routes>
                 <Route path='/' element={<LandingPage/>} />
